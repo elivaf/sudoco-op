902c9e16b41a92ecc170650c6e43ee64c8173edf
elivaf <72341606+elivaf@users.noreply.github.com>
2025-02-18 04:29:40 +1000

    change port

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ def handle_make_move(data):
                     emit('send_board', {'board': sudoku_game.values}, broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port, debug=True)
